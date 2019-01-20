9f273b02cd97483da2c8d95f6dc91ca2637ca7ba
claudiahamidah <claudiahamidah@gmail.com>
2019-01-20 13:08:52 +0700

    added unit test

---
+++ SortNameTest/SortByLastNameTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortByLastNameApp;
+
+namespace SortNameTest
+{
+    [TestClass]
+    public class SortByLastNameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SortName sort = new SortName();
+            String pathFileUnsorted = "C:\\Users\\Claudia\\source\\repos\\SortByLastNameApp\\SortNameTest\\FileUnsorted\\unsorted-name.txt";
+            String pathExpectedResult = "C:\\Users\\Claudia\\source\\repos\\SortByLastNameApp\\SortNameTest\\FileUnsorted\\testExpected.txt";
+
+            String text = System.IO.File.ReadAllText(pathFileUnsorted);
+
+            String expectedResult = System.IO.File.ReadAllText(pathExpectedResult);
+
+            String sorted = sort.SortByLastname(text, pathFileUnsorted);
+
+            Assert.AreEqual(expectedResult, sorted, "The result is not same as expected");
+
+
+
+        }
+    }
+}
